79f6b481c0d057a823111fe1946cb4557cfadd6a
Cristian Gonzalez Barreto <yersi2288@gmail.com>
2023-06-01 23:36:09 -0500

    Mejora de imagenes

--- rentasmdi/settings.py
+++ rentasmdi/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
