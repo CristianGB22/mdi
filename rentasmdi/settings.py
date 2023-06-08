a467ba8a156c6513978ef88784d4e2cee4541ed2
Cristian Gonzalez Barreto <yersi2288@gmail.com>
2023-06-08 16:10:05 -0500

    quitando /

--- rentasmdi/settings.py
+++ rentasmdi/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'rentasmdi/media')
 
 if not DEBUG:
