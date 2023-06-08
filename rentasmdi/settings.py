7d7a5e01115e9a2fde49abc66bc53804f8d64071
Cristian Gonzalez Barreto <yersi2288@gmail.com>
2023-06-08 15:21:07 -0500

    agregando rentasmdi/media

--- rentasmdi/settings.py
+++ rentasmdi/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'rentasmdi/media')
 
 if not DEBUG:
    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
