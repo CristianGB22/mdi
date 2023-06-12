bbe2e6faee36dcbaaf2e5f62f7fe53c6d5729fca
Cristian Gonzalez Barreto <yersi2288@gmail.com>
2023-06-11 23:33:47 -0500

    sin static_files_dirs

--- rentasmdi/settings.py
+++ rentasmdi/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'media')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR,'media'),'/anuncios/']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
